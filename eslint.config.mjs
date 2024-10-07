e7b1987398003964d3f16aa56f2814fa0a786c4a
Erin Schnabel <erinschnabel@gmail.com>
2024-10-07 12:40:36 -0400

    ⬆️ fix eslint. See #557

---
+++ eslint.config.mjs
@@ -0,0 +1,48 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["**/npm node_modules", "**/build"],
+}, ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "module",
+    },
+
+    rules: {
+        "@typescript-eslint/no-unused-vars": ["error", {
+            args: "none",
+            varsIgnorePattern: "_",
+        }],
+
+        "@typescript-eslint/ban-ts-comment": "off",
+        "no-prototype-builtins": "off",
+        "@typescript-eslint/no-empty-function": "off",
+
+        "@typescript-eslint/no-this-alias": ["error", {
+            allowDestructuring: false,
+            allowedNames: ["self"],
+        }],
+    },
+}];
